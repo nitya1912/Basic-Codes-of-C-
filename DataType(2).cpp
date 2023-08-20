d720c7f9a250bf190af1a0dfe9c196dc186c7803
nitya1912 <137202773+nitya1912@users.noreply.github.com>
2023-08-20 12:45:09 +0530

    Add files via upload

---
+++ DataType(2).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 34;
+    float num2 = 45.96;
+    string word1 = "ENTC";
+    string word2 = "#codekaroinnovatekaro";
+    cout<<"My integer value is: "<<num1<<endl;;
+    cout<<"My float value is: "<<num2<<endl;
+    cout<<"My branch name is: "<<word1<<endl;
+    cout<<"My branch's tagline is: "<<word2<<endl;
+    cout<<sizeof(num1)<<endl;
+    cout<<sizeof(num2)<<endl;
+    cout<<sizeof(word1)<<endl;
+    cout<<sizeof(word2)<<endl;
+}
