f71ca6568086eda5a695a64b8eae7737f0072fe6
nitya1912 <137202773+nitya1912@users.noreply.github.com>
2023-08-20 12:44:39 +0530

    Add files via upload

---
+++ DataTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 34;
+    float num2 = 12.78;
+    string word = "E&TC";
+    char myCharVar = '#';
+    cout<<num1<<endl;
+    cout<<num2<<endl;
+    cout<<word<<endl;
+    cout<<myCharVar<<endl;
+}
